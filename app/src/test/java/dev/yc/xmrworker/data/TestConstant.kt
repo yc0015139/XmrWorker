2cb4876b5cc78a7ed9163f0cf2c932f2e00036ef
yc0015139 <a12125577@gmail.com>
2022-07-12 19:08:14 +0800

    Update to junit 5 and add constants

---
+++ app/src/test/java/dev/yc/xmrworker/data/TestConstant.kt
@@ -0,0 +1,9 @@
+package dev.yc.xmrworker.data
+
+import dev.yc.xmrworker.data.remote.config.SupportXmrConfig
+
+val SUPPORT_XMR_CONFIG = SupportXmrConfig()
+const val ADDRESS =
+    "86mmrEzBQ3RAiiZbEcnJXEZJsuTZE6oSXdwotPxNzEYxZQCfchfNFFYJzGMsii6DLYNxAxW5sGwDBEWvqq9tBsKyBWaKtX1"
+const val IDENTIFIER = "YC-PC"
+const val GLOBAL_ID = "global"
