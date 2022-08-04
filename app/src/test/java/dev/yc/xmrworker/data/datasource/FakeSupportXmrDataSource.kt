fab1f9ced9f4a597d3481a84ff0217bfb7cccf44
yc0015139 <a12125577@gmail.com>
2022-08-04 19:40:51 +0800

    Implement test cases with network error

--- app/src/test/java/dev/yc/xmrworker/data/datasource/FakeSupportXmrDataSource.kt
+++ app/src/test/java/dev/yc/xmrworker/data/datasource/FakeSupportXmrDataSource.kt
@@ -4,14 +4,21 @@ import dev.yc.xmrworker.data.remote.ApiResult
 import dev.yc.xmrworker.model.MinerData
 
 class FakeSupportXmrDataSource(
-    private val testData: Map<String, MinerData>,
+    private val testData: Map<String, MinerData> = mapOf(),
+    private val apiResult: ApiResult<Any> = ApiResult.Success(Any()),
 ) : SupportXmrDataSource {
 
     override suspend fun fetchIdentifiers(address: String?): ApiResult<List<String>> {
-        return ApiResult.Success(testData.keys.toList())
+        return when (apiResult) {
+            is ApiResult.Success -> ApiResult.Success(testData.keys.toList())
+            else -> apiResult as ApiResult<List<String>>
+        }
     }
 
-    override suspend fun fetchEachMinerDataById(address: String?, id: String): ApiResult<MinerData> {
+    override suspend fun fetchEachMinerDataById(
+        address: String?,
+        id: String,
+    ): ApiResult<MinerData> {
         return ApiResult.Success(
             result = testData.getValue(id)
         )
--- app/src/test/java/dev/yc/xmrworker/data/repository/SupportXmrRepositoryImplTest.kt
+++ app/src/test/java/dev/yc/xmrworker/data/repository/SupportXmrRepositoryImplTest.kt
@@ -3,14 +3,18 @@ package dev.yc.xmrworker.data.repository
 import dev.yc.xmrworker.data.ADDRESS
 import dev.yc.xmrworker.data.IDENTIFIER
 import dev.yc.xmrworker.data.datasource.FakeSupportXmrDataSource
+import dev.yc.xmrworker.data.remote.ApiResult
 import dev.yc.xmrworker.model.MinerData
 import dev.yc.xmrworker.ui.mineritem.MinerState
 import dev.yc.xmrworker.ui.mineritem.MinerUiState
+import dev.yc.xmrworker.utils.ApiUtil
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle
@@ -18,9 +22,11 @@ import org.junit.jupiter.api.TestInstance.Lifecycle
 @TestInstance(Lifecycle.PER_CLASS)
 class SupportXmrRepositoryImplTest {
     private lateinit var repository: SupportXmrRepository
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
     private lateinit var testData: Map<String, MinerData>
 
-    @BeforeAll
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @BeforeEach
     fun setup() {
         // Arrange
         testData = mapOf(
@@ -36,7 +42,7 @@ class SupportXmrRepositoryImplTest {
 
         repository = SupportXmrRepositoryImpl(
             dataSource = FakeSupportXmrDataSource(testData),
-            dispatcher = UnconfinedTestDispatcher(),
+            dispatcher = testDispatcher,
         )
     }
 
@@ -54,6 +60,7 @@ class SupportXmrRepositoryImplTest {
 
     @Test
     fun fetchMiners_minersAreCurrent() = runTest {
+        // Arrange
         val expected = listOf(
             MinerUiState.Success(
                 minerState = testData.getValue(IDENTIFIER).run {
@@ -72,6 +79,50 @@ class SupportXmrRepositoryImplTest {
 
         flow.collect { actual ->
             // Assert
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun fetchMiners_minerFetchingWithErrorInBadRequest() = runTest {
+        // Arrange
+        repository = SupportXmrRepositoryImpl(
+            dataSource = FakeSupportXmrDataSource(
+                apiResult = ApiResult.Error(ApiUtil.BAD_REQUEST)
+            ),
+            dispatcher = testDispatcher
+        )
+        val expected = listOf(
+            MinerUiState.Error
+        )
+
+        // Action
+        val flow = repository.fetchMiners(ADDRESS)
+
+        // Assert
+        flow.collect { actual ->
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun fetchMiners_minerFetchingWithException() = runTest {
+        // Arrange
+        repository = SupportXmrRepositoryImpl(
+            dataSource = FakeSupportXmrDataSource(
+                apiResult = ApiResult.Exception
+            ),
+            dispatcher = testDispatcher
+        )
+        val expected = listOf(
+            MinerUiState.Exception
+        )
+
+        // Action
+        val flow = repository.fetchMiners(ADDRESS)
+
+        // Assert
+        flow.collect { actual ->
             assertEquals(expected, actual)
         }
     }
